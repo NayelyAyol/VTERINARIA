8e666e52ad40d3f6bece9a5e595258f19cf94485
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-12-17 15:38:32 -0500

    Fallos en el login emplementado

--- api-vet/src/routers/paciente_route.js
+++ api-vet/src/routers/paciente_route.js
@@ -1,14 +1,18 @@
 import { Router} from "express";
-import { actualizarPaciente,detallePaciente,eliminarPaciente,listarPacientes, registrarPaciente } from "../controllers/paciente_controller.js";
+import { actualizarPaciente,detallePaciente,eliminarPaciente,listarPacientes, registrarPaciente, loginPropietario } from "../controllers/paciente_controller.js";
 import { verificarTokenJWT } from "../middlewares/JWT.js";
 
 const router = Router()
+
+//Hay que tener en cuenta el orden de las rutas, ya que si una ruta es mas general que otra,
+//la mas general debe ir al final para que no interfiera con las demas rutas mas especificas
+
+router.post('/paciente/login',loginPropietario)
 
 router.post('/paciente/registro',  verificarTokenJWT,registrarPaciente)
 router.get('/pacientes',verificarTokenJWT,listarPacientes)
 router.get("/paciente/:id",verificarTokenJWT, detallePaciente)
 router.delete('/paciente/eliminar/:id',verificarTokenJWT,eliminarPaciente)
 router.put("/paciente/actualizar/:id", verificarTokenJWT, actualizarPaciente)
-
 
 export default router
